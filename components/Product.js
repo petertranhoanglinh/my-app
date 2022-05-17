80c2ecf36941e0fd575a2d86902dae940522ec87
petertranhoanglinh <petertranhoanglinh@gmail.com>
2022-05-17 18:52:02 +0700

    1
    1

--- components/Product.js
+++ components/Product.js
@@ -1,6 +1,26 @@
 function Product(){
     return (
-        <div></div>
+       
+        <div>
+            
+            <div class="row">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style={{width:"150px", height:"50px"}}>
+              <div className="thumbnail">
+                    <img  src='https://shopapple.vn/wp-content/uploads/2021/07/iphone-12-mau-tim-purple-2021.jpg' alt="iphone 13"/>
+                    <div className="caption">
+                        <h3>Iphone 12 plus</h3>
+                        <p>
+                            250000
+                        </p>
+                        <p>
+                            <a href="#" className="btn btn-primary">Mua hàng</a>
+                        </p>
+                    </div>
+             </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
