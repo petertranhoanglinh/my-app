6fa760adeccc3f48d9f892244f7a0ce38d65cdd8
petertranhoanglinh <73209234+petertranhoanglinh@users.noreply.github.com>
2022-05-31 11:43:50 +0700

    1
    1

--- components/Balance.js
+++ components/Balance.js
@@ -1,191 +1,257 @@
 import React from "react";
 import Url from "./Url";
 class Balance extends React.Component {
-    // Constructor
-    constructor(props) {
-        super(props);
-        this.state = {
-            balances: [],
-            DataisLoaded: false,
-            'quantitySend': 0,
-            'contract': '',
-            addCoin: '',
-            searchCoin: ''
-        };
-    }
-    handleClick = (event) => {
-        // alert(quantityCoin)
-        this.setState({ [event.target.name]: event.target.value })
+  // Constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      balances: [],
+      DataisLoaded: false,
+      quantitySend: 0,
+      contract: "",
+      addCoin: "",
+      searchCoin: "",
+      showHideButton: true,
+    };
+  }
+  handleClick = (event) => {
+    // alert(quantityCoin)
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    }
+  withdraw = (quantityCoin, coinId) => {
+    if (window.confirm("Do you want to send coins?")) {
+      if (this.state.quantitySend > quantityCoin) {
+        alert(
+          "The amount of coins sent cannot be greater than the amount available"
+        );
+        return false;
+      }
+      var raw = JSON.stringify({
+        contract: this.state.contract,
+        coinId: coinId,
+        quantity: this.state.quantitySend,
+      });
 
-    withdraw = (quantityCoin, coinId) => {
-        if (window.confirm('Do you want to send coins?'))
-        {     
-            if (this.state.quantitySend > quantityCoin) {
-                alert('The amount of coins sent cannot be greater than the amount available')
-                return false;
-            }
-            var raw = JSON.stringify({
-                "contract": this.state.contract,
-                "coinId": coinId,
-                "quantity": this.state.quantitySend
-            });
-    
-            var requestOptions = {
-                method: 'POST',
-                headers: Url.headersList,
-                body: raw,
-                redirect: 'follow'
-            };
-    
-            fetch(Url.URL_REST + "api/account/withdraw", requestOptions)
-                .then(response => {
-                    if (response.ok) {
-                        return response.json()
-                    }
-                })
-                .then(result => {
-                    if (result.returnMessage === 'fail') {
-                        alert('address does not exist');
-                    } else {
-                        alert(result.returnMessage);
-                        this.componentDidMount();
-                    }
-                })
-                .catch(error => console.log('error', error));
-        }
-        else
-        {
+      var requestOptions = {
+        method: "POST",
+        headers: Url.headersList,
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch(Url.URL_REST + "api/account/withdraw", requestOptions)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((result) => {
+          if (result.returnMessage === "fail") {
+            alert("address does not exist");
+          } else {
+            alert(result.returnMessage);
+            this.componentDidMount();
+          }
+        })
+        .catch((error) => console.log("error", error));
+    } else {
+      return false;
+    }
+  };
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch(Url.URL_REST + "api/account/balance/*", {
+      method: "GET",
+      headers: Url.headersList,
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({
+          balances: json,
+          DataisLoaded: true,
+          showHideButton: true,
+        });
+      });
+  }
+  setPram = (event) => {
+    this.setState({ [event.target.name]: event.target.value.trim() });
+  };
+  searchCoin = () => {
+    if (this.state.searchCoin === "") {
+      this.componentDidMount();
+    } else {
+      fetch(Url.URL_REST + "api/account/balance/" + this.state.searchCoin, {
+        method: "GET",
+        headers: Url.headersList,
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json);
+          this.setState({
+            balances: json,
+            DataisLoaded: true,
+            showHideButton: false,
+          });
+        });
+    }
+  };
+
+  addCoin = () => {
+    var coinId = this.state.addCoin;
+
+    var raw = "";
+
+    var requestOptions = {
+      method: "POST",
+      headers: Url.headersList,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(Url.URL_REST + "api/account/add/" + coinId, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.statusCode === 1030) {
+          alert(result.message);
           return false;
+        } else {
+          alert("suscess");
         }
+        this.componentDidMount();
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
-    }
-    // ComponentDidMount is used to
-    // execute the code
-    componentDidMount() {
-        fetch(Url.URL_REST + "api/account/balance/*", {
-            method: "GET",
-            headers: Url.headersList
-        }).then((res) => res.json())
-            .then((json) => {
-                console.log(json);
-                this.setState({
-                    balances: json,
-                    DataisLoaded: true
-                });
-            })
-    }
-    setPram = (event) => {
-        this.setState({ [event.target.name]: event.target.value.trim() });
-    }
-    searchCoin =() =>{
-        fetch(Url.URL_REST + "api/account/balance/"+this.state.searchCoin, {
-            method: "GET",
-            headers: Url.headersList
-        }).then((res) => res.json())
-            .then((json) => {
-                console.log(json);
-                this.setState({
-                    balances: json,
-                    DataisLoaded: true
-                });
-            })
-    }
-
-    addCoin = () => {
-
-        var coinId = this.state.addCoin;
-        
-
-        var raw = "";
-
-        var requestOptions = {
-            method: 'POST',
-            headers: Url.headersList,
-            body: raw,
-            redirect: 'follow'
-        };
-
-        fetch(Url.URL_REST + "api/account/add/" + coinId, requestOptions)
-            .then(response => response.json())
-            .then(result => {
-                console.log(result)
-                if (result.statusCode === 1030) {
-                    alert(result.message)
-                    return false;
-                } else {
-                    alert('suscess');
-                }
-                this.componentDidMount();
-            })
-            .catch(error => {
-                console.log('error', error)
-            });
-    }
-
-
-    render() {
-        const { DataisLoaded, balances } = this.state;
-        if (!DataisLoaded) return <div>
-            <h6 className="text-title-cl"> Plesea login... </h6> </div>;
-        else
-            return (
-                <div>
-                    <input type='text' name='searchCoin' onChange={this.setPram} placeholder='Search Balace' />
-                    <button type="button" class="btn btn-default" aria-label="Left Align" onClick={this.searchCoin} >
-                        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                    </button>
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th className="text-th-cl">AccountId</th>
-                                <th className="text-th-cl">CoinId</th>
-                                <th className="text-th-cl">Contract</th>
-                                <th className="text-th-cl">Amount of coins</th>
-                                <th className="text-th-cl">Time Update</th>
-                                <th className="text-th-cl">Cost</th>
-                                <th className="text-th-cl">Total amount (USD)</th>
-                                <th className="text-th-cl">Amount of coin sent</th>
-                                <th className="text-th-cl">Contract to send</th>
-                                <th className="text-th-cl">Send</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                balances.map(
-                                    coin =>
-                                        <tr key={coin.id}>
-                                            <td className="text-td-cl">{coin.accountId}</td>
-                                            <td className="text-td-cl">{coin.coinId}</td>
-                                            <td className="text-td-cl">{coin.contract}</td>
-                                            <td className="text-td-cl">{coin.quantityReal}</td>
-                                            <td className="text-td-cl">{coin.timeUpdate}</td>
-                                            <td className="text-td-cl">{coin.price}</td>
-                                            <td className="text-td-cl">{coin.value}</td>
-                                            <td className="text-td-cl">
-                                                <input type="number" name="quantitySend" class="form-control" onChange={this.handleClick} />
-                                            </td>
-                                            <td className="text-td-cl">
-                                                <input type="text" name="contract" class="form-control" onChange={this.handleClick} />
-                                            </td>
-                                            <td className="text-td-cl">
-                                                <button onClick={() => this.withdraw(coin.quantityReal, coin.coinId)} type="button" class="btn btn-default" aria-label="Left Align">
-                                                   <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-                    <input type='text' name='addCoin' onChange={this.setPram} placeholder='add Balance' />
-                    <button onClick={this.addCoin} type="button" class="btn btn-default" aria-label="Left Align">
-                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                    </button>
-                </div>
-            );
-    }
+  render() {
+    const { DataisLoaded, balances } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h6 className="text-title-cl"> Plesea login... </h6>{" "}
+        </div>
+      );
+    else
+      return (
+        <div>
+          <input
+            type="text"
+            name="searchCoin"
+            onChange={this.setPram}
+            placeholder="Search Balace"
+          />
+          <button
+            type="button"
+            class="btn btn-default"
+            aria-label="Left Align"
+            onClick={this.searchCoin}
+          >
+            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+          </button>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th className="text-th-cl">AccountId</th>
+                <th className="text-th-cl">CoinId</th>
+                <th className="text-th-cl">Contract</th>
+                <th className="text-th-cl">Amount of coins</th>
+                <th className="text-th-cl">Time Update</th>
+                <th className="text-th-cl">Cost</th>
+                <th className="text-th-cl">Total amount (USD)</th>
+                {this.state.showHideButton ? (
+                  <th className="text-th-cl">Amount of coin sent</th>
+                ) : null}
+                {this.state.showHideButton ? (
+                  <th className="text-th-cl">Contract to send</th>
+                ) : null}
+                {this.state.showHideButton ? (
+                  <th className="text-th-cl">Send</th>
+                ) : null}
+              </tr>
+            </thead>
+            <tbody>
+              {balances.map((coin) => (
+                <tr key={coin.id}>
+                  <td className="text-td-cl">{coin.accountId}</td>
+                  <td className="text-td-cl">{coin.coinId}</td>
+                  <td className="text-td-cl">{coin.contract}</td>
+                  <td className="text-td-cl">{coin.quantityReal}</td>
+                  <td className="text-td-cl">{coin.timeUpdate}</td>
+                  <td className="text-td-cl">{coin.price}</td>
+                  <td className="text-td-cl">{coin.value}</td>
+                  {this.state.showHideButton ? (
+                    <td className="text-td-cl">
+                      <input
+                        type="number"
+                        name="quantitySend"
+                        class="form-control"
+                        onChange={this.handleClick}
+                      />
+                    </td>
+                  ) : null}
+                  {this.state.showHideButton ? (
+                    <td className="text-td-cl">
+                      <input
+                        type="text"
+                        name="contract"
+                        class="form-control"
+                        onChange={this.handleClick}
+                      />
+                    </td>
+                  ) : null}
+                  {this.state.showHideButton ? (
+                    <td className="text-td-cl">
+                      <input
+                        type="text"
+                        name="contract"
+                        class="form-control"
+                        onChange={this.handleClick}
+                      />
+                    </td>
+                  ) : null}
+                  {this.state.showHideButton ? (
+                    <td className="text-td-cl">
+                      <button
+                        onClick={() =>
+                          this.withdraw(coin.quantityReal, coin.coinId)
+                        }
+                        type="button"
+                        class="btn btn-default"
+                        aria-label="Left Align"
+                      >
+                        <span
+                          class="glyphicon glyphicon-triangle-right"
+                          aria-hidden="true"
+                        ></span>
+                      </button>
+                    </td>
+                  ) : null}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <input
+            type="text"
+            name="addCoin"
+            onChange={this.setPram}
+            placeholder="add Balance"
+          />
+          <button
+            onClick={this.addCoin}
+            type="button"
+            class="btn btn-default"
+            aria-label="Left Align"
+          >
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+          </button>
+        </div>
+      );
+  }
 }
 
 export default Balance;
--- components/ProductDetail.js
+++ components/ProductDetail.js
@@ -1,31 +1,28 @@
 import { useParams } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Url from "./Url"
 const ProductDetail = () =>{
     const {pdtId} = useParams();
-    const [state, setValue] = useState({pdtID:pdtId});
+    const [state, setValue] = useState({pdtID:pdtId, product:{}});
    // setValue({pdtID: pdtId});
-   
+    useEffect(() =>{
+        fetch(Url.URL_REST+"api/product/getPdt/"+pdtId, {
+            method: "GET",
+            headers: Url.headersList
+            }).then((res) => res.json())
+            .then((json) => {
+                setValue({
+                    product: json
+                });
+        })
+    })
 
     return (
         <div>
            
-           <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-                
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th ></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <input type="text" value={state.pdtID} onChange={(ev) => setValue({...state, pdtID: ev.target.value})} />
-                        </tr>
-                    </tbody>
-                </table>
-                
-               
-           </div>
+           
+                <span>Product Id </span>:<span>{state.product.pdtId}</span>
+          
            
             
         </div>
