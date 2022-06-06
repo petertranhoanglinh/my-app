eb2b6b837c5b84ec27e5d8324049a12c877bcbba
petertranhoanglinh <petertranhoanglinh@gmail.com>
2022-06-06 08:33:42 +0700

    1
    1

--- components/Util.js
+++ components/Util.js
@@ -1,5 +1,6 @@
-//var URL_REST = "https://tienaodemo12.herokuapp.com/";
+// var URL_REST = "https://tienaodemo12.herokuapp.com/";
 var URL_REST = "http://localhost:8089/";
+//var URL = "https://lweb5.herokuapp.com/"; //on server
 var URL = "http://localhost:3000/";
 const token = localStorage.getItem("token");
 const AuthStr = "Bearer " + token;
